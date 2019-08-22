21b72773b70a3085ef4edba415617c6fc5b6dda9
Carlos Moretti <carlosmoretti2019@gmail.com>
2019-08-21 21:25:19 -0300

    Commit de arquivos!

---
+++ Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var con = factory.CreateConnection())
+            {
+                using (var channel = con.CreateModel())
+                {
+                    channel.QueueDeclare("Hello", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) => {
+                        var body = ea.Body;
+                        Entities.Pessoa pes = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.Pessoa>(Encoding.UTF8.GetString(body));
+                        Console.WriteLine("[x] Received {0}", string.Format("{0} | {1}", pes.Nome, pes.Idade));
+                    };
+
+                    channel.BasicConsume(queue: "Hello", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Press [Enter] to exit");
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var con = factory.CreateConnection())
+            {
+                using (var channel = con.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    for(int i =0; i < 15000; i++)
+                    {
+                        Entities.Pessoa pes = new Entities.Pessoa()
+                        {
+                            Idade = new Random().Next(),
+                            Nome = "Moretti"
+                        };
+
+                        var body = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(pes));
+
+                        channel.BasicPublish("", "Hello", true, null, body);
+                        Console.WriteLine("Enviou {0}", body);
+                        System.Threading.Thread.Sleep(3000);
+                    }
+
+                    Console.WriteLine("[x] Mensagem enviada!");
+                }
+            }
+
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
